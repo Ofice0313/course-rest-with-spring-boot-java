4d95307312fb17d9be9edc2e737b9abcd15b8527
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2026-04-23 14:36:10 +0200

    project created

---
+++ rest-with-spring-boot-java/src/test/java/devcaleb/rest_with_spring_boot_java/StartUpTests.java
@@ -0,0 +1,13 @@
+package devcaleb.rest_with_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
